b8f9f6ad7a27605b2346b4aa039ad83330e4915d
Dejan1990 <dejan.simo@hotmail.com>
2024-01-22 15:20:01 +0100

    Composition Api - basic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+
+onMounted(() => {
+  alert("Hello from HomeView")
+})
+
+setTimeout(() => {
+  message.value = 'This is new message'
+}, 2000)
+
+let message = ref("Hello world")
+
+
+
+// export default {
+//   components: { TheWelcome },
+
+//   data() {
+//     return {
+//       message: "Hello world"
+//     }
+//   },
+
+//   mounted() {
+//     alert("I have been mounted.")
+//   },
+// }
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
   </main>
 </template>
