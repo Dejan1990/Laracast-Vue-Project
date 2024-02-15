e56242155f88e5a8956f422f37ffe9295596b1ac
Dejan1990 <dejan.simo@hotmail.com>
2024-02-15 13:00:16 +0100

    Add wather to change value in LocalStorage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 let food = ref(localStorage.getItem('food'));
 let age = ref(localStorage.getItem('age'));
+
+watch(food, (val) => {
+  write('food', val)
+})
 
 function write(key, value) {
   localStorage.setItem(key, value)
@@ -17,10 +21,10 @@ setTimeout(() => {
   <main>
     <TheWelcome />
     <p>
-      What's your favorite food? <input type="text" v-model="food" @input="write('food', food)">
+      What's your favorite food? <input type="text" v-model="food">
     </p>
     <p>
-      What's your favorite food? <input type="text" v-model="age" @input="write('age', age)">
+      What's your favorite food? <input type="text" v-model="age">
     </p>
   </main>
 </template>
