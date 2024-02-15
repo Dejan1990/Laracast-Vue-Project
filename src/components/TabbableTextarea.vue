b73777b5763f8242c9c1c155b0fc84f0108dae85
Dejan1990 <dejan.simo@hotmail.com>
2024-02-15 21:25:52 +0100

    Add component for textarea

---
+++ src/components/TabbableTextarea.vue
@@ -0,0 +1,18 @@
+<script setup>
+    function onTabPress(e) {
+        let t = e.target
+        let val = t.value,
+            start = t.selectionStart,
+            end = t.selectionEnd;
+
+      
+        t.value = val.substring(0, start) + "\t" + val.substring(end)
+    
+        t.selectionStart = t.selectionEnd = start + 1
+    }
+</script>
+
+<template>
+    <textarea @keydown.tab.prevent="onTabPress"></textarea>
+</template>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,11 @@
 <script setup>
-  function onTabPress(e) {
-    // we can get rid of the addEventListener, because we're already doing that at the point we call this function
-    let t = e.target
-    
-      // get caret position/selection
-      let val = t.value,
-        start = t.selectionStart,
-        end = t.selectionEnd;
-
-      // set textarea value to: text before caret + tab + text after caret
-      t.value = val.substring(0, start) + "\t" + val.substring(end)
-
-      // put caret at right position again
-      t.selectionStart = t.selectionEnd = start + 1
-  }
-  
+  import TabbableTextarea from "@/components/TabbableTextarea.vue"
 </script>
 
 <template>
   <main>
     <form>
-      <textarea @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px;">Hi there</textarea>
+      <TabbableTextarea style="width: 100%; height: 300px;" />
     </form>
   </main>
 </template>
