f0e2e726da584b114d5dd69734089b6de92fb6e4
Dejan1990 <dejan.simo@hotmail.com>
2024-02-16 16:05:43 +0100

    Add CounterView page

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,16 @@
 </script>
 
 <template>
-  
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/counter">Counter</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+      </nav>
+    </div>
+  </header>
 
   <RouterView />
 </template>
