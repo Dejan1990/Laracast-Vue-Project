4e64123196b68dd8bb6809b955c77b55a092f9a9
Dejan1990 <dejan.simo@hotmail.com>
2024-02-16 13:06:05 +0100

    Dependency Injection With Provide and Inject - reacive data

--- src/components/Quiz/Quiz.vue
+++ src/components/Quiz/Quiz.vue
@@ -8,9 +8,11 @@
     import QuizHeader from '@/components/Quiz/QuizHeader.vue';
     import QuizQuestion from '@/components/Quiz/QuizQuestion.vue';
     import QuizFooter from '@/components/Quiz/QuizFooter.vue';
-    import {provide} from 'vue'
+    import {provide, ref} from 'vue'
 
     // defineProps({quiz: Object});
 
-    provide('key', 'Hello there');
+    let name = ref('John Doe')
+
+    provide('key', name);
 </script>
