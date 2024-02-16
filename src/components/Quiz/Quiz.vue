80a3eb6ec737e4b8226643852ac0a80b8b0da48e
Dejan1990 <dejan.simo@hotmail.com>
2024-02-16 13:45:57 +0100

    Dependency Injection With Provide and Inject - last part

--- src/components/Quiz/Quiz.vue
+++ src/components/Quiz/Quiz.vue
@@ -1,5 +1,4 @@
 <template>
-    <h1>{{ name }}</h1>
     <QuizHeader />
     <QuizQuestion />
     <QuizFooter />
@@ -9,14 +8,9 @@
     import QuizHeader from '@/components/Quiz/QuizHeader.vue';
     import QuizQuestion from '@/components/Quiz/QuizQuestion.vue';
     import QuizFooter from '@/components/Quiz/QuizFooter.vue';
-    import {provide, ref} from 'vue'
+    import {provide} from 'vue'
 
-    // defineProps({quiz: Object});
+    let props =  defineProps({quiz: Object});
 
-    let name = ref('Jane Doe')
-
-    provide('name', {
-        name,  // because the key and the value are the same name: name
-        changeName: () => name.value = 'Changed'
-    });
+    provide('quiz', props.quiz)
 </script>
--- src/components/Quiz/QuizFooterLinks.vue
+++ src/components/Quiz/QuizFooterLinks.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button @click.prevent="changeName">{{ name }}</button>
+        <h5>{{ quiz.name }}</h5>
 
         <ul>
             <li><a href="#">Get a Job</a></li>
@@ -12,7 +12,5 @@
 <script setup>
 import {inject} from 'vue'
 
-// defineProps({quiz: Object});
-
-let {name, changeName} = inject('name');
+let quiz = inject('quiz');
 </script>
